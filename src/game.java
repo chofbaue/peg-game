a4cbdbe8b6879661cb87e7d1e23f2c3b14c73846
Cameron Hofbauer <70484504+chofbaue@users.noreply.github.com>
2024-01-11 23:09:01 -0500

    New function: Build board from STDIN Revised: counting number of moves based on board shape. Revised: Printing board with dynamically-adjustable boarder. Increased commenting frequency for readability.

--- src/game.java
+++ src/game.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * game - A class which holds the solving methods to solve a given peg board,
@@ -9,6 +10,7 @@ public class game {
 
     static boolean solutionFound = false;
     static int iterationCount = 0;
+    static boolean isPerfectBoard = false;
 
     public static int getStartingX(Axis a, Direction d, int destX) {
 
@@ -63,7 +65,7 @@ public class game {
                 int middleX = (x + furthestX)/2;
                 int middleY = (y + furthestY)/2;
 
-                if (board[y][x] != false || board[furthestY][furthestX] != true || board[middleY][middleX] != true) {
+                if (board[y][x] || board[furthestY][furthestX] != true || board[middleY][middleX] != true) {
                     continue;
                 }
 
@@ -123,6 +125,8 @@ public class game {
     /**
      * numMoves - A simplified method of <code>numPegs</code>, which adds the length of every row
      * (number of possible x-values on each row), iterating on each row (the y-axis).
+     * This method should be used specifically on "perfect boards".
+     *
      * @param board The board to solve
      * @return Maximum number of moves to achieve an optimal solution.
      */
@@ -208,69 +212,53 @@ public class game {
 
         // Check iteration counter
         if (iterationCount++ % 1000000 == 0) {
-            System.out.println("Iteration: " + iterationCount);
-            System.out.println("Depth: " + level);
+            System.out.println("Iteration: " + iterationCount + "\nDepth: " + level);
             printBoard(board);
         }
 
-        // Base case: check if no more moves can be made
-        if (movesTaken.size() == numMoves(board)) {
-            //if (numPegs(board) == 1) {
+        // Base case: check if no more moves can be made.
+        // Base case uses two different algorithms: one is a "shortcut" if the
+        // board is a shape of an equilateral triangle, and the other counts
+        // each individual space occupied by a peg.
+        if ((isPerfectBoard && movesTaken.size() == numMoves(board)) ||
+                (!isPerfectBoard && movesTaken.size() == numPegs(board))) {
             System.out.println("SOLUTION FOUND:\nFinal Iteration: " + iterationCount);
             solutionFound = true;
             return movesTaken;
         }
 
         // Get the list of all possible moves
-        ArrayList<Move> allMoves;
+        ArrayList<Move> allPossibleMoves;
+        allPossibleMoves = getAllMoves(board);
 
-
-        //ArrayList<Move> discoveredMoves;
-
-        //// Iterate through all possible spaces on the board
-        //for (int i = 0; i < board.length; i++) {
-        //    for (int j = 0; j < board[i].length; j++) {
-
-        //        // A spot that is occupied by a peg is TRUE
-        //        // Meaning that a move cannot be performed at
-        //        // this space.
-        //        if (board[i][j]) {
-        //            continue;
-        //        }
-
-        //        // Discover all valid move options
-        //        discoveredMoves = getValidMoves(i, j, board);
-
-        //        // Add all discovered moves into "allMoves"
-        //        allMoves.addAll(discoveredMoves);
-        //    }
-        //}
-        allMoves = getAllMoves(board);
-
-        // Randomize all moves list
-        //Collections.shuffle(allMoves);
-
-        sortByMaxMoves(board, allMoves);
+        // Sort allPossibleMoves list
+        //Collections.shuffle(allPossibleMoves);    // Randomize allPossibleMoves list
+        //sortByMaxMoves(board, allPossibleMoves);    // Sort allPossibleMoves list by max order
+        allPossibleMoves = sortByMaxMoves(board, allPossibleMoves);
 
         // From the list of all moves, recursively call
         // this method by making all possible moves
-        ArrayList<Move> returned;
+        ArrayList<Move> nextMoves;
 
-        for (Move m : allMoves) {
+        for (Move m : allPossibleMoves) {
             // Take move
             takeMove(board, m, movesTaken);
 
             // Find next moves
-            returned = solveHelper(board, movesTaken, level+1);
+            // The nextMoves arraylist can be null, however this is the edge case
+            // where all move choices are exhausted with no further solution.
+            nextMoves = solveHelper(board, movesTaken, level+1);
 
-            // If the solution was found in the returned list of solutions,
+            // If the solution was found in the nextMoves list of solutions,
             // return the list of solutions
-            if (solutionFound)  return returned;
+            if (solutionFound)  return nextMoves;
 
             // Undo this move
             undoMove(board, m, movesTaken);
         }
 
+        // After exhausting through allPossibleMoves list, there are no other
+        // options to make. In this case, return null.
         return null;
     }
 
@@ -313,13 +301,14 @@ public class game {
             undoMove(board, m);
         }
 
-        //System.out.println(Arrays.toString(sortedMoves.toArray()));
-
         return sortedMoves;
 
     }
 
     /**
+     *
+     * insertByNumMoves - mutates inputted lists by inserting each
+     * element as a "max heap" with respect to numMoves
      *
      * @param m
      * @param numMoves
@@ -332,7 +321,7 @@ public class game {
             ArrayList<Move> sortedMoves,
             ArrayList<Integer> sortedNumMoves) {
 
-        if (sortedMoves.size() == 0) {
+        if (sortedMoves.isEmpty()) {
             sortedMoves.add(m);
             sortedNumMoves.add(numMoves);
             return;
@@ -353,13 +342,28 @@ public class game {
         sortedNumMoves.add(index, numMoves);
     }
 
+    /**
+     * solveBoard - A method to solve the board and print out detailed
+     * steps (per move) on how to solve the board
+     * @param board
+     * @return
+     */
     public static boolean solveBoard(boolean[][] board) {
 
         ArrayList<Move> moves;  // Keep a list of moves taken
         boolean[][] originalBoard = copyBoard(board);  // Keep the original board state if a solution is found.
 
+        // Print out the starting board
+        System.out.println(" Starting Board:");
+        printBoard(board);
 
-        // Solve board
+        // Check if the board is "perfect", meaning it takes a shape of an equilateral triangle
+        // As described in the method's JavaDoc, this will select the proper algorithm to execute,
+        // where a perfect board has a theoretically more optimized algorithm.
+        setIsPerfectBoard(board);
+
+        // Solve the board (utilizing recursion).
+        // Get the list of moves (or null if no solution).
         moves = solveHelper(board, new ArrayList<>(), 0);
 
         // If the board is unsolveable, there is no solution
@@ -392,6 +396,30 @@ public class game {
         return true;
     }
 
+    /**
+     * setIsPerfectBoard - checks if the board takes the shape of an equilateral triangle.
+     * This determines the algorithm to check the number of moves, where a "perfect board"
+     * has a theoretically faster algorithm.
+     *
+     * @param board
+     */
+    private static void setIsPerfectBoard(boolean[][] board) {
+
+        int prev = -1;
+
+        for (boolean[] row : board) {
+
+            if (prev != -1 && Math.abs(row.length - prev) != 1) {
+                isPerfectBoard = false;
+                return;
+            }
+
+            prev = row.length;
+        }
+
+        isPerfectBoard = true;
+    }
+
     private static boolean[][] copyBoard(boolean[][] board) {
         boolean[][] newBoard = new boolean[board.length][];
 
@@ -399,9 +427,7 @@ public class game {
 
             newBoard[i] = new boolean[board[i].length];
 
-            for (int j = 0; j < board[i].length; j++) {
-                newBoard[i][j] = board[i][j];
-            }
+            System.arraycopy(board[i], 0, newBoard[i], 0, board[i].length);
         }
 
         return newBoard;
@@ -409,9 +435,15 @@ public class game {
 
     public static void main(String[] args) {
 
-        boolean[][] selectedBoard;
+        final boolean stdinInput = true;
+        final String terminatingStr = "q";
 
-        Scanner s = new Scanner(System.in);
+        boolean[][] selectedBoard;
+        Scanner s;
+        StringBuilder builder;
+        String line;
+
+        //In the future: Check args[] to determine whether to read from STDIN or not.
 
         boolean[][] smallBoard = {
                 {true},
@@ -439,27 +471,142 @@ public class game {
 
         boolean[][] megaBoard = {
                 {true},
-                {true, false},
-                {true, true, true},
+                {true, true},
+                {false, true, true},
                 {true, true, true, true},
                 {true, true, true, true, true},
                 {true, true, true, true, true, true},
                 {true, true, true, true, true, true, true}
         };
 
-        selectedBoard = largeBoard;
+        // If stdinInput flag is selected, input board from STDIN using Util.Scanner
+        if (stdinInput) {
+            builder = new StringBuilder();
+            s = new Scanner(System.in);
 
+            System.out.println("""
+                ====================
+                Please enter the board in the form of 0's and 1's, terminated by a 'q' on a separate line.
+                For example:
+                0
+                11
+                111
+                1111
+                q
+                ===================="""
+            );
 
-        System.out.println(" Starting Board:");
+            while (s.hasNextLine()) {
+                // Extract input from scanner
+                line = s.next();
 
-        printBoard(selectedBoard);
+                // Check if the input string is the terminating 'q'.
+                if (line.compareTo(terminatingStr) == 0) {
+                    break;
+                }
+
+                // Append the line to the builder, followed by a newline char.
+                builder.append(line).append("\n");
+            }
+
+            // It is most appropriate to close the scanner.
+            s.close();
+
+            // selectedBoard - Defines which board will be solved.
+            selectedBoard = createBoardFromInput(builder.toString());
+
+            // Proceed to solve the board.
+            solveBoard(selectedBoard);
+
+            return;
+        }
+
+        // selectedBoard - Defines which board will be solved.
+        selectedBoard = megaBoard;
+
+        // Proceed to solve the board.
         solveBoard(selectedBoard);
+    }
+
+    private static boolean[][] createBoardFromInput(String s) {
+
+        boolean[][] rows = new boolean[0][];
+        boolean[] col = new boolean[0];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // In the future: Refactor this switch statement for readability
+            switch (c) {
+
+                case '\n' -> {
+                    // Expand rows table by 1 row
+                    boolean[][] newRows = new boolean[rows.length + 1][];
+
+                    // Copy contents of rows table into newRows table
+                    System.arraycopy(rows, 0, newRows, 0, rows.length);
+
+                    // Insert col array into open position in rows table
+                    newRows[newRows.length - 1] = col;
+
+                    // Set rows table as newRows
+                    rows = newRows;
+
+                    // Set col to be an empty array
+                    col = new boolean[0];
+                }
+
+                case ' ' -> { /* Empty, continue through loop */ }
+
+                case '1' -> {
+                    // Expand col array by 1 to accommodate for new value
+                    boolean[] newCol = new boolean[col.length + 1];
+
+                    // Copy contents of col array to newCol array
+                    System.arraycopy(col, 0, newCol, 0, col.length);
+
+                    // Set value at end of array to be true, since the
+                    // input specified this position to be occupied by a peg
+                    newCol[newCol.length - 1] = true;
+
+                    // Set col array as newCol
+                    col = newCol;
+                }
+
+                case '0' -> {
+                    // Expand col array by 1 to accommodate for new value
+                    boolean[] newCol = new boolean[col.length + 1];
+
+                    // Copy contents of col array to newCol array
+                    System.arraycopy(col, 0, newCol, 0, col.length);
+
+                    // Set value at end of array to be false, since the
+                    // input specified this position to be unoccupied
+                    newCol[newCol.length - 1] = false;
+
+                    // Set col array as newCol
+                    col = newCol;
+                }
+
+                default -> {
+                    System.err.println("Error: Invalid Character! (" + c + ") is not a valid board input");
+                    System.exit(1);
+                }
+            }
+        }
+
+
+        return rows;
     }
 
     private static void printBoard(boolean[][] board) {
         int magicNum = board.length;
 
-        System.out.println("-----------------");
+        // Construct the boarder which surrounds the game board.
+        // This boarder stretches as needed to accommodate for the board size.
+        StringBuilder dynamicBoarder = getDynamicBoarder(board);
+
+        System.out.println(dynamicBoarder);
         System.out.println();
 
         StringBuilder line = new StringBuilder();
@@ -468,9 +615,7 @@ public class game {
 
             line.append("   ");
 
-            for (int j = 0; j < magicNum - i; j++) {
-                line.append(" ");
-            }
+            line.append(" ".repeat(magicNum - i));
 
             for (int j = 0; j < board[i].length; j++) {
                 line.append(board[i][j]?"| ":"o ");
@@ -481,7 +626,27 @@ public class game {
         }
 
         System.out.println();
-        System.out.println("-----------------");
+        System.out.println(dynamicBoarder);
         System.out.println();
+    }
+
+    /**
+     * getDynamicBoarder - Constructs the dynamic boarder used by printBoard().
+     * This boarder stretches as needed to accommodate for the size of the board.
+     * 
+     * @param board - The board to construct the boarder with.
+     * 
+     * @return A StringBuilder Object that contains the "fitted" boarder String.
+     */
+    private static StringBuilder getDynamicBoarder(boolean[][] board) {
+        final String boarderUnit = "--";
+        final String boarderFront = "---";
+        final String boarderTail = "----";
+        StringBuilder dynamicBoarder = new StringBuilder(boarderFront);
+
+        dynamicBoarder.append(boarderUnit.repeat(board[board.length - 1].length));
+
+        dynamicBoarder.append(boarderTail);
+        return dynamicBoarder;
     }
 }
