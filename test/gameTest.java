80aa8da605a7c23b3fded40a36aa73fba6819c80
Cameron Hofbauer <70484504+chofbaue@users.noreply.github.com>
2024-01-23 23:34:21 -0500

    Updated Tests

--- test/gameTest.java
+++ test/gameTest.java
@@ -1,14 +1,17 @@
-//import org.junit.Test;
-
 import org.junit.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
+
 public class gameTest {
 
     @Test
     public void testGame() {
+
+        // This board should have 2 possible moves to make
+        final int boardMoves = 2;
         boolean[][] board = {
                 {false},
                 {true, false},
@@ -16,6 +19,8 @@ public class gameTest {
                 {false, true, false, false}
         };
 
+        // This board should have 1 possible move to make
+        final int outcome1Moves = 1;
         boolean[][] outcome1 = {
                 {false},
                 {false, false},
@@ -23,6 +28,8 @@ public class gameTest {
                 {true, true, false, false}
         };
 
+        // This board should not have moves to make
+        final int outcome2Moves = 0;
         boolean[][] outcome2 = {
                 {true},
                 {false, false},
@@ -31,8 +38,14 @@ public class gameTest {
         };
 
         ArrayList<Move> allMoves = game.getAllMoves(board);
+        ArrayList<Move> allMoves1 = game.getAllMoves(outcome1);
+        ArrayList<Move> allMoves2 = game.getAllMoves(outcome2);
         System.out.println(Arrays.toString(allMoves.toArray()));
+        System.out.println(Arrays.toString(allMoves1.toArray()));
+        System.out.println(Arrays.toString(allMoves2.toArray()));
 
-        //assertEquals()
+        assertEquals(allMoves.size(), boardMoves);
+        assertEquals(allMoves1.size(), outcome1Moves);
+        assertEquals(allMoves2.size(), outcome2Moves);
     }
 }
